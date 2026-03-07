0b0f2ba90f475299006ff22167cfa40071858615
MiguelCSerpa <miguel.serpa@sptech.school>
2026-03-07 14:09:11 -0300

    Desenvolvimento v1 Inovacao

---
+++ etl/src/main/java/sptech/school/LambdaExtracao.java
@@ -0,0 +1,72 @@
+package sptech.school;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import java.nio.charset.StandardCharsets;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+
+public class LambdaExtracao implements RequestHandler<Object, String> {
+
+    private final String NOME_BUCKET = "testeetlcrmetais";
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+
+        try {
+
+            // Criar cliente HTTP
+            HttpClient cliente = HttpClient.newHttpClient();
+
+            // Chamar endpoint ETL
+            HttpRequest requisicao = HttpRequest.newBuilder()
+                    .uri(URI.create("http://192.168.0.16:8080/api/etl/full-extract"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = cliente.send(
+                    requisicao,
+                    HttpResponse.BodyHandlers.ofString()
+            );
+
+            String body = response.body();
+
+            // Configurar nome do arquivo
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String dataHoje = LocalDateTime.now().format(formatter);
+
+            String nomeArquivo = "Relatório-" + dataHoje + ".json";
+
+            S3Client s3 = S3Client.builder().build();
+
+            // Criar requisição para upload
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(NOME_BUCKET)
+                    .key(nomeArquivo)
+                    .build();
+
+            // Enviar arquivo para o S3
+            s3.putObject(
+                    putObjectRequest,
+                    RequestBody.fromString(body, StandardCharsets.UTF_8)
+            );
+
+            return "Arquivo enviado com sucesso: " + nomeArquivo;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro na execução: " + e.getMessage();
+        }
+    }
+}
---
+++ etl/src/main/java/sptech/school/Main.java
@@ -0,0 +1,14 @@
+package sptech.school;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LambdaExtracao lambda = new LambdaExtracao();
+
+        String resultado = lambda.handleRequest(null, null);
+
+        System.out.println(resultado);
+
+    }
+}
